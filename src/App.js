9eaa521011f09bc5f8ae7f3a945babfdd9df59a5
David Mrnustik <david.mrnustik@hotmail.com>
2017-11-08 20:40:45 +0100

    feat: Basic component structure
    Create Adisson, Event and  Market Component. Apply bootstrap component.
    Fill with dummy text. Add basic css component for Event.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,59 @@
 import React, { Component } from 'react';
-import './App.css';
+import { customStyles as styles } from './CustomStyles';
+import { Grid, Row, Col, Button } from 'react-bootstrap';
 
-class App extends Component {
+class AdissonApp extends Component {
   render() {
     return (
-      <div className="App">
-        Addison
+      <div className="AddisonApp">
+        <Grid>
+          <Row>
+            <Col md={4}>
+              <Event />
+            </Col>
+            <Col md={4}>
+              <Event />
+            </Col>
+            <Col md={4}>
+              <Event />
+            </Col>
+          </Row>
+        </Grid>
       </div>
     );
   }
 }
 
-export default App;
+class Event extends Component {
+  render() {
+    return(
+      <div>
+        <div style={styles.alignCenter}>Man. United <span style={styles.paddingVs}>vs</span> Chelsea</div>
+        <Market title='To WIN' />
+        <Market title='To Score First' />
+      </div>
+    )
+  }
+}
+
+class Market extends Component {
+  render() {
+    const { title } = this.props;
+
+    return(
+      <div>
+        {title}
+        <Row>
+          <Col xs={6}>
+            <Button>Man. United 1.2</Button>
+          </Col>
+          <Col xs={6} style={styles.alignRight}>
+            <Button>Chelsea 2.2</Button>
+          </Col>
+        </Row>
+      </div>
+    )
+  }
+}
+
+export default AdissonApp;
---
+++ src/CustomStyles.js
@@ -0,0 +1,12 @@
+export const customStyles = {
+  alignRight: {
+    textAlign: 'right'
+  },
+  alignCenter: {
+    textAlign: 'center'
+  },
+  paddingVs: {
+    paddingLeft: 30,
+    paddingRight: 30
+  }
+}
