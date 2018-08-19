48bf4bf2858927de2f98f7eb37183b1c706d3759
David Mrnustik <david.mrnustik@hotmail.com>
2018-08-19 18:39:20 +0200

    chore: React scripts eject
    - ejecting react project
    - create jest.json
    - update setupTests with mockXHR and XDomainRequest

--- src/setupTests.js
+++ src/setupTests.js
@@ -3,9 +3,16 @@ import Adapter from 'enzyme-adapter-react-16';
 
 configure({ adapter: new Adapter() });
 
+const mockXHR = {
+  open: jest.fn(),
+  send: jest.fn(),
+};
 const localStorageMock = {
   getItem: jest.fn(),
   setItem: jest.fn(),
   clear: jest.fn()
 };
 global.localStorage = localStorageMock
+
+window.XMLHttpRequest = jest.fn(() => mockXHR);
+window.XDomainRequest = jest.fn(() => mockXHR);
